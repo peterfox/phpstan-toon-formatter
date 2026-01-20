2a8370b0bf9dedd0bb5c596e1a5a5a1c2f445207
Peter Fox <peter.fox@peterfox.me>
2026-01-20 11:14:46 +0000

    Setup
    # Conflicts:
    #	.github/workflows/fix-php-code-style-issues-pint.yml
    #	.github/workflows/run-tests-pest.yml

---
+++ src/ToonErrorFormatter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PeterFox\PhpStanToonFormatter;
+
+use HelgeSverre\Toon\Toon;
+use PHPStan\Command\AnalysisResult;
+use PHPStan\Command\ErrorFormatter\ErrorFormatter;
+use PHPStan\Command\Output;
+
+class ToonErrorFormatter implements ErrorFormatter
+{
+    public function formatErrors(AnalysisResult $analysisResult, Output $output): int
+    {
+        $errorsArray = [
+            'totals' => [
+                'errors' => count($analysisResult->getNotFileSpecificErrors()),
+                'file_errors' => count($analysisResult->getFileSpecificErrors()),
+            ],
+            'files' => [],
+            'errors' => [],
+        ];
+
+        foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+            $file = $fileSpecificError->getFile();
+            if (! isset($errorsArray['files'][$file])) {
+                $errorsArray['files'][$file] = [
+                    'errors' => 0,
+                    'messages' => [],
+                ];
+            }
+            $errorsArray['files'][$file]['errors']++;
+
+            $message = [
+                'message' => $fileSpecificError->getMessage(),
+                'line' => $fileSpecificError->getLine(),
+                'ignorable' => $fileSpecificError->canBeIgnored(),
+            ];
+
+            if ($fileSpecificError->getTip() !== null) {
+                $message['tip'] = $fileSpecificError->getTip();
+            }
+
+            if ($fileSpecificError->getIdentifier() !== null) {
+                $message['identifier'] = $fileSpecificError->getIdentifier();
+            }
+
+            $errorsArray['files'][$file]['messages'][] = $message;
+        }
+
+        foreach ($analysisResult->getNotFileSpecificErrors() as $notFileSpecificError) {
+            $errorsArray['errors'][] = $notFileSpecificError;
+        }
+
+        $output->writeRaw(Toon::encode($errorsArray));
+
+        return $analysisResult->hasErrors() ? 1 : 0;
+    }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace PeterFox\PhpStanToonFormatter\Tests;
 
 use PHPUnit\Framework\TestCase;
 
